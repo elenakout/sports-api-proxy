6893e08e306b4cfc412807ab05a36285c38a7fcf
elenakout <elenakout@gmail.com>
2020-03-16 16:43:22 +0200

    [next-game]

--- src/api/controllers/next-game.js
+++ src/api/controllers/next-game.js
@@ -1,7 +1,34 @@
 const fetch = require('node-fetch');
 
-let cache = null;
+const BASE_URL = 'https://api.football-data.org/v2';
+
+let cache = {};
 let lastCacheTime = null;
+
+async function nextGame() {
+  if (cache && lastCacheTime > Date.now() - 60 * 60 * 12) {
+    console.log('chase returned');
+    return cache;
+  }
+
+  const response = await fetch(
+    `${BASE_URL}/teams/108/matches?status=SCHEDULED`,
+    {
+      headers: {
+        'X-Auth-Token': process.env.API_TOKEN,
+      },
+    }
+  );
+
+  const nGame = await response.json();
+  lastCacheTime = Date.now();
+  cache.nGame = nGame;
+  cache.status = 'ok';
+  console.log('call returned');
+  return nGame;
+}
+
+module.exports = nextGame;
 
 // async function scrapeTable() {
 //   if (cache && lastCacheTime > Date.now() - (1000 * 60 * 10)) {
--- src/api/fdata.js
+++ src/api/fdata.js
@@ -1,36 +1,30 @@
-const { Router } = require("express");
-const fetch = require("node-fetch");
+const { Router } = require('express');
+const fetch = require('node-fetch');
+
+const nextGame = require('./controllers/next-game');
 
 const router = Router();
 
-const BASE_URL = "https://api.football-data.org/v2";
+const BASE_URL = 'https://api.football-data.org/v2';
 
 //Next game
 
-router.get("/next-game", async (req, res) => {
-  const response = await fetch(
-    `${BASE_URL}/teams/108/matches?status=SCHEDULED`,
-    {
-      headers: {
-        "X-Auth-Token": process.env.API_TOKEN
-      }
-    }
-  );
+router.get('/next-game', async (req, res) => {
+  const data = await nextGame();
 
-  const json = await response.json();
-  res.json(json.matches[0]);
+  res.json(data);
 });
 
 // Match info
 // http://api.football-data.org/v2/matches/279440
 
-router.get("/match/:id", async (req, res) => {
+router.get('/match/:id', async (req, res) => {
   const matchId = req.params.id;
 
   const response = await fetch(`${BASE_URL}/matches/${matchId}`, {
     headers: {
-      "X-Auth-Token": process.env.API_TOKEN
-    }
+      'X-Auth-Token': process.env.API_TOKEN,
+    },
   });
 
   const json = await response.json();
@@ -39,13 +33,13 @@ router.get("/match/:id", async (req, res) => {
 
 // teams info
 
-router.get("/teams/:id", async (req, res) => {
+router.get('/teams/:id', async (req, res) => {
   const teamId = req.params.id;
 
   const response = await fetch(`${BASE_URL}/teams/${teamId}`, {
     headers: {
-      "X-Auth-Token": process.env.API_TOKEN
-    }
+      'X-Auth-Token': process.env.API_TOKEN,
+    },
   });
 
   const json = await response.json();
@@ -55,13 +49,13 @@ router.get("/teams/:id", async (req, res) => {
 // Standings
 // http://api.football-data.org/v2/competitions/SA/standings
 
-router.get("/standings", async (req, res) => {
+router.get('/standings', async (req, res) => {
   const teamId = req.params.id;
 
   const response = await fetch(`${BASE_URL}/competitions/SA/standings`, {
     headers: {
-      "X-Auth-Token": process.env.API_TOKEN
-    }
+      'X-Auth-Token': process.env.API_TOKEN,
+    },
   });
 
   const json = await response.json();
