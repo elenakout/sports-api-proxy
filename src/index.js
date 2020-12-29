b1abc7856441582f883ff0c740d2c11474410555
elenakout <elenakout@gmail.com>
2020-12-29 20:45:12 +0200

    fix: cors error removed cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const helmet = require('helmet');
 
 
@@ -12,7 +12,7 @@ const fdata = require('./api/routes/fdata');
 const email = require('./api/routes/email');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(morgan('common'));
 
 app.use(helmet());
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
 
 app.get('/', (req, res) => {
   res.json({
