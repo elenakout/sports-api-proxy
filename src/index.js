ae8b01552eab6e82649c4cb4069cf270a4b494ce
elenakout <elenakout@gmail.com>
2020-12-29 20:32:44 +0200

    fix: missing next()

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ app.use(helmet());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 })
 
 app.get('/', (req, res) => {
