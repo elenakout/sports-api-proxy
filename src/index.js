f571b9536c1491ca569dbdfe7950ea462803ba6e
elenakout <elenakout@gmail.com>
2020-12-30 13:08:14 +0200

    refactor: set original state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 
 
 require('dotenv').config();
@@ -15,14 +15,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('common'));
 
-// app.use(helmet());
+app.use(helmet());
 
 app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-})
 
 app.get('/', (req, res) => {
   res.json({
